cc4cb4c0fa0ec1e902106af68b60a5947fcbecbd
gr2nade <gr2nade@gmail.com>
2025-03-05 18:25:11 +0900

    Ollama api tags

--- api/ollama_api.py
+++ api/ollama_api.py
@@ -2,12 +2,12 @@ import requests
 import os
 from dotenv import load_dotenv
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL = "http://localhost:11434/"
 
 
-def chatCompletion(messages, model="mistral", temperature=0.7):
-    """Ollama API 호출"""
-    url = OLLAMA_URL
+def apiGenerate(messages, model="mistral", temperature=0.7):
+    """Ollama API Generate 호출"""
+    url = OLLAMA_URL + "api/generate"
     payload = {
         "model": model,
         "messages": messages,
@@ -16,8 +16,20 @@ def chatCompletion(messages, model="mistral", temperature=0.7):
 
     try:
         response = requests.post(url, json=payload)
-        response_data = response.json()
-        return response_data.get("response", "오류발생")
+        data = response.json()
+        return data.get("response", "[ERROR] OLLAMA API : Generate Response (response)")
     except Exception as e:
         print(f"오류 발생: {e}")
+        return None
+
+def apiTags () :
+    """Ollama API Tags 호출"""
+    url  = OLLAMA_URL + "api/tags"
+    
+    try :
+        response = requests.post(url)
+        data = response.json()
+        return data.get("models", "[ERROR] OLLAMA API : Tags Response (models)")
+    except Exception as e: 
+        print (f"[ERROR] OLLAMA API : {e}")
         return None
