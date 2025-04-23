99dec05a4d132d56dee36ea905d206095307c004
gr2nade <gr2nade@gmail.com>
2025-04-23 23:25:50 +0900

    20250423 route test task

--- dashboard/routes/home.py
+++ dashboard/routes/home.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from dashboard.tasks import test_task
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def home():
     return render_template("index.html")
+
+@home_bp.route("test_task")
+def start_task():
+    seconds = int(request.args.get('sec', 5))
+    task = test_task.long_task(seconds)
+    return jsonify({"task_id" : task.id})
