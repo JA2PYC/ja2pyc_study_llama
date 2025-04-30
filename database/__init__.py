f388d6c51d5d2e225a8c6a35a7612f78ab63d180
gr2nade <gr2nade@gmail.com>
2025-04-30 19:58:23 +0900

    20250430 module structure

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,6 @@
 # database/__init__.py
 print("[INIT] ℹ️ database 패키지 초기화")
 
-from .db_connector import db_client, DatabaseClient
+from .connector import db_client, DatabaseClient
 
 __all__ = ["db_client", "DatabaseClient"]
