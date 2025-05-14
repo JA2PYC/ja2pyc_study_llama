08e13492d0a8ad76ba55b49f683548c72202efd5
gr2nade <gr2nade@gmail.com>
2025-05-14 22:20:09 +0900

    20250514 engine

--- database/init_db.py
+++ database/init_db.py
@@ -1,6 +1,8 @@
 from .connector import engine
 from .connector import Base
+# from .connector import DatabaseClient
 from .entities import user_entity
 
 def init_db():
+    # engine = DatabaseClient.get_engine()
     Base.metadata.create_all(bind=engine)
