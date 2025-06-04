67e558ed3c64d7eefc10cb6f88779561b0f2a828
gr2nade <gr2nade@gmail.com>
2025-06-04 22:43:54 +0900

    20250604 abs db client

--- database/config.py
+++ database/config.py
@@ -7,10 +7,10 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PROT = os.getenv("DB_PORT", "3306")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "llmaquarium")
 
-DATABASE_URL_DEFAULT = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PROT}/"
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PROT}/{DB_NAME}"
+DATABASE_URL_DEFAULT = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 STATUS_FILE = "db_status.json"
